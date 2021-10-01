4a81b366ebbe2183f16c7dd421c67fa30fe70f57
Aviv Slobodkin <lovodkin93@cs.huji.ac.il>
2021-10-01 22:03:25 +0300

    update en-ko and en-ru challenges

---
+++ challenge/en-ko/preprocess_challenges_pretrained.sh
@@ -0,0 +1,138 @@
+#!/bin/bash
+#SBATCH --mem=30g
+#SBATCH -c4
+#SBATCH --time=10:0:0
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
+#SBATCH --mail-user=leshem.choshen@mail.huji.ac.il
+#SBATCH --output=/cs/labs/oabend/lovodkin93/encoder_masking_data/pre_process_same_scene_mask/slurm/preprocess_challenge-%j.out
+#SBATCH --wckey=strmt
+
+source /cs/snapless/oabend/borgr/envs/tg/bin/activate
+
+# this sample script preprocesses a sample corpus, including tokenization,
+# truecasing, and subword segmentation.
+# for application to a different language pair,
+# change source and target prefix, optionally the number of BPE operations,
+# source /cs/snapless/oabend/borgr/envs/SSMT/bin/activate
+# source /cs/snapless/oabend/borgr/envs/non_proficient/bin/activate
+script_dir=`dirname $0`
+max_len=120
+main_dir=$script_dir/..
+
+src=en
+trg=ko
+data_dir=/cs/labs/oabend/lovodkin93/encoder_masking_data/pre_process_same_scene_mask/en_ko/data/challenge/
+
+capitalized_langs=(en de fr es ru et kk tr "fi")
+if [[ " ${capitalized_langs[@]} " =~ " ${trg} " ]]; then
+    trg_tc=".tc"
+else
+  trg_tc=""
+fi
+if [[ " ${capitalized_langs[@]} " =~ " ${src} " ]]; then
+  src_tc=".tc"
+else
+  src_tc=""
+fi
+
+# scripts directory of moses decoder: http://www.statmt.org/moses/
+# you do not need to compile moses; a simple download is sufficient
+#moses_scripts=/home/bhaddow/moses.new/dist/977e8ea/scripts
+moses_scripts=/cs/snapless/oabend/borgr/SSMT/preprocess/mosesdecoder/scripts
+
+#scripts for subword segmentation: https://github.com/rsennrich/subword-nmt
+#bpe_scripts=/home/bhaddow/tools/subword-nmt
+bpe_scripts=/cs/snapless/oabend/borgr/SSMT/preprocess/subword-nmt
+
+filter_lines="filter_lines.py"
+# filtering charateristics
+min_length=1
+max_length=1000
+ratio=5
+alignment_score="-180"
+fast_align=/cs/snapless/oabend/borgr/SSMT/fast_align/build/fast_align
+
+
+nematus_home=/cs/snapless/oabend/borgr/TG/
+
+#jieba word segmentation utility: https://pypi.python.org/pypi/jieba/
+#this is only required for Chinese
+zh_segment_home=/mnt/baldur0/tramooc/tools/jieba
+
+unescape=/cs/snapless/oabend/borgr/SSMT/preprocess/unescape.py
+
+##minimum number of times we need to have seen a character sequence in the training text before we merge it into one unit
+##this is applied to each training text independently, even with joint BPE
+#bpe_threshold=50
+
+src_model_dir=/cs/snapless/oabend/borgr/SSMT/preprocess/model/30.11.20/
+trg_model_dir=/cs/snapless/oabend/borgr/SSMT/preprocess/model/30.11.20/
+out_data_dir=/cs/labs/oabend/lovodkin93/encoder_masking_data/pre_process_same_scene_mask/en_ko/preprocess/30.11.20/challenge
+if [ ! -d $out_data_dir ]; then
+  mkdir $out_data_dir
+fi
+
+# tokenize
+# for prefix in Books_reorder5 newstest2013
+pattern=".*unesc.*"
+marked=".*mark.*"
+has_digits="[0-9]" # || [[ $prefix =~ $has_digits ]]
+for filename in $data_dir/*;
+ do
+   filename=$(basename -- "$filename")
+   prefix="${filename%.*}"
+   if [[ $prefix =~ $pattern ]]  || [[ $prefix =~ marked ]] || [ -f $out_data_dir/$prefix.tok.$src ]; then
+#      :
+      echo "File found ${out_data_dir}/${prefix}.tok.$src, skipping preprocessing."
+   else
+    echo "Processing $prefix"
+
+    cp $data_dir/$prefix.$src $out_data_dir/$prefix.cln.$src
+    cp $data_dir/$prefix.$trg $out_data_dir/$prefix.cln.$trg
+    python /cs/snapless/oabend/borgr/SSMT/preprocess/drop_long_lines.py -s $out_data_dir/$prefix.cln.$src -t $out_data_dir/$prefix.cln.$trg --max_len $max_len
+#    python /cs/snapless/oabend/borgr/SSMT/preprocess/preprocess.py -t $trg -s $src -f $data_dir -n "$prefix" -o "$prefix.cln" -d "$out_data_dir"
+#    python $unescape $trg
+    cat $out_data_dir/$prefix.cln.shrt.$src | \
+    $moses_scripts/tokenizer/normalize-punctuation.perl -l $src | \
+    $moses_scripts/tokenizer/tokenizer.perl -no-escape -a -l $src > $out_data_dir/$prefix.tok.$src
+
+    cat $out_data_dir/$prefix.cln.shrt.$trg | \
+    $moses_scripts/tokenizer/normalize-punctuation.perl -l $trg | \
+    $moses_scripts/tokenizer/tokenizer.perl -no-escape -a -l $trg > $out_data_dir/$prefix.tok.$trg
+
+
+    #apply filtering
+    python3 $filter_lines $out_data_dir/$prefix.clean $src $trg $out_data_dir/$prefix.tok.$src $out_data_dir/$prefix.tok.$trg --ratio $ratio --min $min_length --max $max_length --fast_align $fast_align --fast_score $alignment_score
+    rm -rf $out_data_dir/tmp
+
+    # apply truecaser (dev/test files)
+    if [[ " ${capitalized_langs[@]} " =~ " ${src} " ]]; then
+    $moses_scripts/recaser/truecase.perl -model $src_model_dir/truecase-model.train.$src < $out_data_dir/$prefix.clean.$src > $out_data_dir/$prefix$src_tc.$src
+    else
+      cp $out_data_dir/$prefix.clean.$src $out_data_dir/$prefix$src_tc.$src
+    fi
+    if [[ " ${capitalized_langs[@]} " =~ " ${trg} " ]]; then
+    $moses_scripts/recaser/truecase.perl -model $trg_model_dir/truecase-model.train.$trg < $out_data_dir/$prefix.clean.$trg > $out_data_dir/$prefix$trg_tc.$trg
+    else
+      cp $out_data_dir/$prefix.clean.$trg $out_data_dir/$prefix$trg_tc.$trg
+    fi
+#
+#    # apply BPE for joint bpes
+#
+#    $bpe_scripts/apply_bpe.py -c $src_model_dir/$trg$src.bpe --glossaries "=" < $out_data_dir/$prefix$src_tc.$src > $out_data_dir/$prefix.bpe.$src
+#    $bpe_scripts/apply_bpe.py -c $trg_model_dir/$trg$src.bpe --glossaries "=" < $out_data_dir/$prefix$trg_tc.$trg > $out_data_dir/$prefix.bpe.$trg
+
+    # apply BPE for separate bpes
+
+#    $bpe_scripts/apply_bpe.py -c $src_model_dir/bpe.model.$src --glossaries "=" < $out_data_dir/$prefix$src_tc.$src > $out_data_dir/$prefix.bpe.$src
+#    $bpe_scripts/apply_bpe.py -c $trg_model_dir/bpe.model.$trg --vocabulary $out_data_dir/../vocab.clean.unesc.tok$trg_tc.bpe.$trg < $out_data_dir/$prefix$trg_tc.$trg > $out_data_dir/$prefix.bpe.$trg
+    $bpe_scripts/apply_bpe.py -c $src_model_dir/${src}_bpe.model --glossaries "=" < $out_data_dir/$prefix${src_tc}.$src > $out_data_dir/$prefix.bpe.$src
+    $bpe_scripts/apply_bpe.py -c $src_model_dir/${trg}_bpe.model --glossaries "=" < $out_data_dir/$prefix${trg_tc}.$trg > $out_data_dir/$prefix.bpe.$trg
+#    $bpe_scripts/apply_bpe.py -c $trg_model_dir/${trg}_bpe.model --vocabulary $out_data_dir/../vocab.clean.unesc.tok$trg_tc.bpe.$trg < $out_data_dir/$prefix$trg_tc.$trg > $out_data_dir/$prefix.bpe.$trg
+
+   fi
+ done
+cp $0 $out_data_dir/preprocess_challenges_pretrained.sh
+# # build network dictionary
+# $nematus_home/data/build_dictionary.py $out_data_dir/corpus.bpe.$src $out_data_dir/corpus.bpe.$trg
+
